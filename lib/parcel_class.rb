492ebadb35b2505976257a63025e97f247a4f441
Guest User <student@epicodus.com>
2015-08-10 16:50:50 -0700

    parcel_class passes second spec

--- lib/parcel_class.rb
+++ lib/parcel_class.rb
@@ -9,4 +9,12 @@ class Parcel
   define_method(:volume) do
     volume = @height.*(@width).*(@length)
   end
+  define_method(:cost_to_ship) do
+    self.volume()
+      if volume >=(50)
+        shipping_cost = 5
+      else
+        0
+      end
+  end    
 end
--- spec/parcel_class_spec.rb
+++ spec/parcel_class_spec.rb
@@ -3,9 +3,15 @@ require('parcel_class')
 
 describe(Parcel) do
   describe('#volume') do
-  it("enters dimensions and returns volume of parcel") do
-  test_volume = Parcel.new(10, 15, 5, 1)
-  expect(test_volume.volume()).to(eq(750))
+    it("enters dimensions and returns volume of parcel") do
+    test_volume = Parcel.new(10, 15, 5, 1)
+    expect(test_volume.volume()).to(eq(750))
+    end
   end
+  describe('#cost_to_ship') do
+    it("returns shipping cost to user based on dimensions") do
+    test_cost_to_ship = Parcel.new(2,2,2,1)
+    expect(test_cost_to_ship.cost_to_ship()).to(eq(0))
+    end
   end
 end
